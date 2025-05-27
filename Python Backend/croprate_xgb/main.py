79b1b652e2addb48f3ef0bd024bb5b8525519df9
Suryadeep36 <gohilsuryadeep3101@gmail.com>
2025-05-27 21:06:06 +0530

    fix stupid coors undefined error

--- Python Backend/croprate_xgb/main.py
+++ Python Backend/croprate_xgb/main.py
@@ -2,6 +2,7 @@
 
 from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel
+from fastapi.middleware.cors import CORSMiddleware
 from pretrained_croprate_xgb import predict_croprate
 
 app = FastAPI()
